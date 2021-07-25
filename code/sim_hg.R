5c35a3d9a96071b840b21486a7656d5c47ca189d
miaoyan wang <saplingwang@gmail.com>
2021-07-25 00:31:29 -0500

    upload

--- code/sim_hg.R
+++ code/sim_hg.R
@@ -29,3 +29,32 @@ result2[i,rep]=CER(hgmod$Cs,res$Cs)
 }
 
 plot(apply(result,1,mean))
+
+
+source("functions_hg.R")
+source("functions_sbm2.R")
+library("RSKC")
+set.seed(1)
+g = 20;
+result=result2=matrix(nrow=5,ncol=1);
+
+n_list=c(50,100,150,200,250);
+
+for(i in 1:5){
+    
+    W = signaltensor(g,10,sym = T,smooth = T)
+    
+    snmod = snmod.block(W,n_list[i],sym = F,diagP = T)
+    k = r = l = g
+    Sig = snmod$Sig
+    obsTensor = snmod$obsTensor
+    Cs = snmod$Cs
+    
+    ini=HSC(obsTensor,k,r,l,sym=F)
+    res =tbmClustering(obsTensor,k,r,l,Cs.init=ini$Cs,Ds.init=ini$Cs,Es.init=ini$Cs,sym = F, diagP = T)
+    result[i]=mean((res$judgeX-Sig)^2)
+    result2[i]=CER(snmod$Cs,res$Cs)
+    
+}
+
+plot(apply(result,1,mean))

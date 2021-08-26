646901e6659674473452d4acd6b5c3ea2e0249ac
Chanwoo Lee <lcw10947@gmail.com>
2021-08-25 20:51:58 -0500

    Update tensor_visualization.R

--- code/tensor_visualization.R
+++ code/tensor_visualization.R
@@ -32,48 +32,6 @@ positionfun=function(d){
 }
 
 
-d1=40 ## number of rows
-d2=40 ## number of columns
-d3=50 ## number of 3rd dimension
-K1=3  ## number of row clusters 
-K2=4  ## number of column clusters 
-K3=5  ## number of clusters in the 3rd dimension
-
-cluster1_index=sample(1:K1,d1,replace=T) ## randomly assign d1 rows to K1 clusters
-cluster2_index=sample(1:K2,d2,replace=T) 
-cluster3_index=sample(1:K3,d3,replace=T)
-
-K1=length(unique(cluster1_index)) ## actual number of clusters in realization; no larger than pre-specified K1
-K2=length(unique(cluster2_index))
-K3=length(unique(cluster3_index))
-
-group_mean=array(runif(K1*K2*K3,-3,3),dim=c(K1,K2,K3)) ## generate group-specific mean
-signal=group_mean[cluster1_index,cluster2_index,cluster3_index] ## generate a stochastic block tensor following the groups-spedific mean
-noise=array(rnorm(d1*d2*d3,0,0.1),dim=c(d1,d2,d3))
-
-plot_tensor(signal+noise) ## plot data tensor
-signal_reorganize=signal[sort(cluster1_index,index=T)$ix,sort(cluster2_index,index=T)$ix,sort(cluster3_index,index=T)$ix] ## re-organize the tensor entries; put entries in the same groups together.
-plot_tensor(signal_reorganize) ## plot the signal tensor with reorganized indices
-
-########################################################################################
-n = 40
-m = 3
-gp1 = graphon1(n,m)
-gp2 = graphon2(n,m)
-gp3 = graphon3(n,m)
-gp4 = graphon4(n,m)
-
-A1 = gp1$A; A2 = gp2$A; A3 = gp3$A; A4 = gp4$A
-P1 = gp1$P; P2 = gp2$P; P3 = gp3$P; P4 = gp4$P
-N3 = array(rnorm(40^3),dim = c(40,40,40))
-plot_tensor(P3)
-rgl.postscript('P3.pdf', fmt = 'pdf')
-plot_tensor(P3+N3)
-rgl.postscript('A3.pdf', fmt = 'pdf')
-
-blk = block(P3+N3,c(9,9,9))
-plot_tensor(blk$Blk_mat)
-rgl.postscript('B3.pdf', fmt = 'pdf')
 
 
 
